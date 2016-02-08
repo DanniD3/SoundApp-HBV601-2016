5a89365f676e95a57ab4595400d59caef8e2e522
DanniD3 <daa11@hi.is>
2016-02-08 21:38:27 +0000

    Git init
    Starter stuff

---
+++ app/src/main/java/thepack/soundapp/SoundClipActivity.java
@@ -0,0 +1,13 @@
+package thepack.soundapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundClipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_clip);
+    }
+}
