984623ce60bc9a1bdd27c5f5d36c27e4760773ea
Jianfei Zheng <jiz2@hi.is>
2016-03-07 17:03:51 +0000

    Setup UploadActivity for content

--- app/src/main/java/thepack/soundapp/UploadActivity.java
+++ app/src/main/java/thepack/soundapp/UploadActivity.java
@@ -1,13 +1,26 @@
 package thepack.soundapp;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UploadActivity extends FragmentActivity {
+
+    private Button uploadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        uploadButton = (Button) findViewById(R.id.login);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(UploadActivity.this, UserActivity.class));
+            }
+        });
     }
 }
