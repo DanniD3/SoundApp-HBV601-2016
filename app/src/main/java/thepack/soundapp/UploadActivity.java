ffd9e0cc1a50be3fb781ee41ba539ed95be04c67
Jianfei Zheng <jiz2@hi.is>
2016-03-11 01:28:41 +0000

    Trying to send large files

--- app/src/main/java/thepack/soundapp/UploadActivity.java
+++ app/src/main/java/thepack/soundapp/UploadActivity.java
@@ -3,17 +3,20 @@ package thepack.soundapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -24,6 +27,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class UploadActivity extends FragmentActivity {
 
@@ -54,15 +58,22 @@ public class UploadActivity extends FragmentActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Upload selected file to REST controller
-//                Toast.makeText(UploadActivity.this, R.string.upload_success, Toast.LENGTH_LONG).show();
+                // Check for MimeType before upload
+                Uri uploadFileUri = Uri.fromFile(uploadFile);
+                String upFileExt = MimeTypeMap.getFileExtensionFromUrl(uploadFileUri.toString());
+                String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(upFileExt);
+                if (mimeType == null || !mimeType.split("/")[0].equalsIgnoreCase("audio")) {
+                    Toast.makeText(UploadActivity.this, R.string.file_invalid, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Check for Internet connection
                 if (!isNetworkAvailableAndConnected()) {
                     Toast.makeText(UploadActivity.this, R.string.network_error, Toast.LENGTH_LONG).show();
                     return;
                 }
 
-//                new GetUpload().execute();
-                new UploadTask(uploadFile).execute();
+                new UploadTask(uploadFile, upFileExt).execute();
             }
         });
     }
@@ -84,7 +95,8 @@ public class UploadActivity extends FragmentActivity {
         return isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
     }
 
-    private class GetUpload extends AsyncTask<Void,Void,String> {
+    // GET request template
+    private class GetStringRequest extends AsyncTask<Void,Void,String> {
 
         @Override
         protected String doInBackground(Void... params) {
@@ -124,9 +136,15 @@ public class UploadActivity extends FragmentActivity {
     private class UploadTask extends AsyncTask<Void,Void,String> {
 
         String encodedFile;
+        String upFileName;
+        String upFileExt;
+//        File uploadFile;
 
-        public UploadTask(File uploadFile) {
+        public UploadTask(File uploadFile, String upFileExt) {
             this.encodedFile = encodeFile(uploadFile);
+            this.upFileName = uploadFile.getName();
+            this.upFileExt = upFileExt;
+//            this.uploadFile = uploadFile;
         }
 
         private String encodeFile(File uploadFile) {
@@ -142,7 +160,10 @@ public class UploadActivity extends FragmentActivity {
                 while ((read = in.read(buffer)) != -1) {
                     out.write(buffer, 0, read);
                 }
-                encodedFile = Base64.encodeToString(out.toByteArray(),Base64.DEFAULT);
+//                encodedFile = out.toString();
+                encodedFile = Base64.encodeToString(out.toByteArray(), Base64.DEFAULT);
+//                Toast.makeText(UploadActivity.this, Integer.toString(out.toByteArray().length), Toast.LENGTH_LONG).show();
+//                return Integer.toString(out.toByteArray().length);
             } catch (FileNotFoundException fnfe) {
                 Toast.makeText(UploadActivity.this, R.string.file_not_found, Toast.LENGTH_LONG).show();
                 fnfe.printStackTrace();
@@ -171,14 +192,44 @@ public class UploadActivity extends FragmentActivity {
                 conn = (HttpURLConnection) new URL(REST_UPLOAD_URL).openConnection();
                 conn.setRequestMethod("POST");
                 conn.setDoOutput(true);
-//                conn.setChunkedStreamingMode(0);
+                conn.setChunkedStreamingMode(2048);
 
-                // Upload encodedFile to server
-                String POST_PARAM = "encodedFile=" + encodedFile;
-                OutputStream sOut = conn.getOutputStream();
-                sOut.write(POST_PARAM.getBytes());
+                // POST Upload encodedFile to server
+                String POST_PARAM =
+                        "encodedFile=" + encodedFile + "&&" +
+                        "fileName=" + upFileName + "&&" +
+                        "fileExt=" + upFileExt;
+                byte[] POST_REQ = POST_PARAM.getBytes();
+//                conn.setFixedLengthStreamingMode(POST_REQ.length);
+                OutputStream sOut = new BufferedOutputStream(conn.getOutputStream());
+                Log.i("UploadActivity","GETBYTES finished, Sending....");
+                Log.i("UploadActivity","GETBYTES finished, Sending....");
+                Log.i("UploadActivity","GETBYTES finished, Sending....");
+                Log.i("UploadActivity","GETBYTES finished, Sending....");
+                Log.i("UploadActivity",POST_PARAM);
+                sOut.write(POST_REQ);
                 sOut.flush();
                 sOut.close();
+
+//                String POST_PARAM =
+//                        "&&" +
+//                        "fileName=" + upFileName + "&&" +
+//                        "fileExt=" + upFileExt;
+//                OutputStream sOut = conn.getOutputStream();
+//                sOut.write("encodedFile=".getBytes());
+//
+//                InputStream fIn = null;
+//                byte[] b = new byte[2048];
+//                fIn = new FileInputStream(uploadFile);
+//                int read = 0;
+//                while ((read = fIn.read(b)) != -1) {
+//                    sOut.write(b, 0, read);
+//                }
+//                fIn.close();
+//
+//                sOut.write(POST_PARAM.getBytes());
+//                sOut.flush();
+//                sOut.close();
 
                 // Get response from server
                 if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
